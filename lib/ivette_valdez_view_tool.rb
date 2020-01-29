f9f1286e2f15c8cf3a14634488c0a587408e9c9b
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-29 19:20:29 +0100

    Initial commit.

---
+++ lib/ivette_valdez_view_tool.rb
@@ -0,0 +1,7 @@
+require "ivette_valdez_view_tool/version"
+require "ivette_valdez_view_tool/renderer"
+
+module IvetteValdezViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ivette_valdez_view_tool/version.rb
@@ -0,0 +1,3 @@
+module IvetteValdezViewTool
+  VERSION = "0.1.0"
+end
